b49a9db10f6123a294fd7cdeb6c5c790af03fcf3
Tomas Santana <tomas.30604530@uru.edu>
2025-11-24 00:37:07 -0400

    delete unused files

--- sum.py
+++
@@ -1,9 +0,0 @@
-from supertracer import SuperTracer
-
-log = open("error_log.txt", "a")
-tracer = SuperTracer(files=[log]) # type: ignore
-    
-def cause_exception():
-    return 1 / 0  # This will raise a ZeroDivisionError
-
-cause_exception()
